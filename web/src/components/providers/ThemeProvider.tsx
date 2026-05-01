eaef89b503835839ff8137bce8d4826a59367d1f
Naufal Aufaa Abyan <naufalaufaaabyan@gmail.com>
2026-05-02 00:43:37 +0700

    fix: force resolve next-themes children type error for react 19

--- web/src/components/providers/ThemeProvider.tsx
+++ web/src/components/providers/ThemeProvider.tsx
@@ -2,10 +2,14 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// FIX: Nge-cast komponen bawaan next-themes menjadi ElementType generik
+// Ini bikin TypeScript berhenti bawel nanyain soal prop 'children'
+const Provider = NextThemesProvider as React.ElementType;
+
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+    <Provider attribute="class" defaultTheme="dark" enableSystem>
       {children}
-    </NextThemesProvider>
+    </Provider>
   );
 }
